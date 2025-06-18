d6f8ed86b63e60745147c971442f2788c348749d
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-06-18 15:22:43 +0200

    feat: add Bukkit module and update project configuration for vanish system

--- surf-vanish-velocity/src/main/kotlin/dev/slne/surf/vanish/velocity/service/VanishServiceImpl.kt
+++ surf-vanish-velocity/src/main/kotlin/dev/slne/surf/vanish/velocity/service/VanishServiceImpl.kt
@@ -1,13 +1,12 @@
 package dev.slne.surf.vanish.velocity.service
 
 import dev.slne.surf.surfapi.core.api.util.mutableObjectSetOf
-import dev.slne.surf.vanish.api.player.VanishPlayer
 import dev.slne.surf.vanish.core.service.VanishService
 
 import org.gradle.internal.impldep.jcifs.dcerpc.UUID
 
 class VanishServiceImpl : VanishService {
-    val vanishedPlayers = mutableObjectSetOf<VanishPlayer>()
+    val vanishedPlayers = mutableObjectSetOf<UUID>()
 
     override fun setVanished(uuid: UUID, vanished: Boolean) {
         TODO("Not yet implemented")
--- surf-vanish-velocity/src/main/kotlin/dev/slne/surf/vanish/velocity/util/packetevents-util.kt
+++ surf-vanish-velocity/src/main/kotlin/dev/slne/surf/vanish/velocity/util/packetevents-util.kt
@@ -3,12 +3,7 @@ package dev.slne.surf.vanish.velocity.util
 import com.github.retrooper.packetevents.PacketEvents
 import com.github.retrooper.packetevents.wrapper.PacketWrapper
 import com.velocitypowered.api.proxy.Player
-import dev.slne.surf.vanish.api.player.VanishPlayer
 
 fun Player.sendPacket(packet: PacketWrapper<*>) {
-    PacketEvents.getAPI().playerManager.sendPacket(this, packet)
-}
-
-fun VanishPlayer.sendPacket(packet: PacketWrapper<*>) {
     PacketEvents.getAPI().playerManager.sendPacket(this, packet)
 }
