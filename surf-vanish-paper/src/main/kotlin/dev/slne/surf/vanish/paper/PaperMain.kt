2c71ac8271f350521cdc18dc42009cc585b1443a
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-06-21 16:46:03 +0200

    feat: register VanishListener for incoming vanish updates in PaperMain

--- surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/PaperMain.kt
+++ surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/PaperMain.kt
@@ -1,10 +1,17 @@
 package dev.slne.surf.vanish.paper
 
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
+import dev.slne.surf.vanish.paper.listener.VanishListener
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class PaperMain() : SuspendingJavaPlugin() {
-
+    override fun onEnable() {
+        Bukkit.getMessenger().registerIncomingPluginChannel(
+            this,
+            "surf-vanish:vanish-updates",
+            VanishListener())
+    }
 }
 
 val plugin get() = JavaPlugin.getPlugin(PaperMain::class.java)
