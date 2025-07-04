6802b736e632ad4eb362f60e715d75c84f23e7df
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-04 09:58:30 +0200

    feat: add PlaceholderAPI dependency and update repositories in build.gradle.kts

--- surf-vanish-paper/build.gradle.kts
+++ surf-vanish-paper/build.gradle.kts
@@ -1,11 +1,17 @@
 import dev.slne.surf.surfapi.gradle.util.registerRequired
+import dev.slne.surf.surfapi.gradle.util.registerSoft
 
 plugins {
     id("dev.slne.surf.surfapi.gradle.paper-plugin")
 }
 
+repositories {
+    maven("https://repo.extendedclip.com/releases/")
+}
+
 dependencies {
     api(project(":surf-vanish-core"))
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 surfPaperPluginApi {
@@ -15,5 +21,6 @@ surfPaperPluginApi {
 
     serverDependencies {
         registerRequired("packetevents")
+        registerSoft("PlaceholderAPI")
     }
 }
